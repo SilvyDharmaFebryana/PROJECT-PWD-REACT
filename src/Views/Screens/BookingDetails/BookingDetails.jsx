38b18ea241588e25d0c81e8958ded42a698474bd
silvyfebryana <silvydharmafebryana@gmail.com>
2020-05-26 16:30:38 +0700

    booking details

---
+++ src/Views/Screens/BookingDetails/BookingDetails.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class BookingDetails extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Booking Details</h1>
+            </div>
+        )
+    }
+}
+
+export default BookingDetails ;
+
