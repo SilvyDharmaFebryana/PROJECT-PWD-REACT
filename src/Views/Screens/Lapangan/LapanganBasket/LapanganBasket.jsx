3ab6348ca6145522725e16bbfce189d9b3f5e126
silvyfebryana <silvydharmafebryana@gmail.com>
2020-05-26 16:44:54 +0700

    lapangan basket

--- src/Views/Screens/Lapangan/LapanganBasket/LapanganBasket.jsx
+++ src/Views/Screens/Lapangan/LapanganBasket/LapanganBasket.jsx
@@ -1,41 +1,40 @@
-import React from "react";
+import React from "react"
 import Axios from "axios";
 import { API_URL } from "../../../../Constants/API";
-import "./LapanganBasket.css";
-import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import "./LapanganBasket.css"
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from "react-router-dom";
 
-class LapanganBasket extends React.Component {
+class LapanganVoli extends React.Component {
+
   state = {
     lapanganBasket: [],
-  };
+  }
 
   getLapanganBasket = () => {
-    Axios.get(`${API_URL}/lapangan`, {
+    Axios.get(`${API_URL}/fields`, {
       params: {
-        category: "basket",
-      },
+        category: "basket"
+      }
     })
       .then((res) => {
-        this.setState({ lapanganBasket: res.data });
+        this.setState({ lapanganBasket: res.data })
         console.log(res.data);
+
       })
       .catch((err) => {
-        console.log(err);
-      });
-  };
+        console.log(err)
+      })
+  }
 
   componentDidMount() {
-    this.getLapanganBasket();
+    this.getLapanganBasket()
   }
 
   renderLapangan = () => {
     return this.state.lapanganBasket.map((val) => {
       return (
-        <Link
-          to={`/lapangan/${val.id}`}
-          style={{ textDecoration: "none", color: "inherit" }}
-        >
+        <Link to={`/lapangan/${val.id}`} style={{ textDecoration: "none", color: "inherit" }}>
           <div className={`kolam-card d-inline-block ml-4 mt-4`}>
             <img
               src={val.image}
@@ -50,36 +49,37 @@ class LapanganBasket extends React.Component {
                       style: "currency",
                       currency: "IDR",
                     }).format(val.price)}
-                    {"      "} / hour
+                    {'      '} / hour
                   </h5>
                 </span>
               </div>
+
             </div>
           </div>
         </Link>
-      );
-    });
-  };
+      )
+    })
+  }
 
   render() {
     return (
       <div>
         <div className="mt-1">
           <Breadcrumb>
-            <BreadcrumbItem active>
-              {" "}
-              <h5 className="font-weight-bolder m-1"> LAPANGAN BASKET </h5>
-            </BreadcrumbItem>
+            <BreadcrumbItem active>  <h5 className="font-weight-bolder m-1" > LAPANGAN BASKET </h5></BreadcrumbItem>
           </Breadcrumb>
         </div>
-        <div className="container">
+        <div className="container" >
+
           <div className="row d-flex flex-wrap justify-content-center ">
-            {this.getLapanganBasket()}
+            {
+              this.renderLapangan()
+            }
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default LapanganBasket;
+export default LapanganVoli;
